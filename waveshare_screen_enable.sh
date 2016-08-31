d39e8e8907564769e4844a322c2b870c46875e30
k073l <krzyswlosek@gmail.com>
2016-08-31 16:32:21 +0200

    Add files via upload

---
+++ waveshare_screen_enable.sh
@@ -0,0 +1,56 @@
+﻿#!/usr/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root, just for sure"
+  exit
+fi
+
+echo "******************"
+echo "*WaveShare enable*"
+echo "******************"
+
+#written by k073l and helpers from StackExchange
+
+echo "First make sure you have WaveShare drivers installed and some free space."
+echo "For sure install on clean system with drivers"
+echo ""
+echo "WaveShare SpotPear 3.2"
+echo "http://www.waveshare.com/wiki/3.2inch_RPi_LCD_(B)"
+echo ""
+echo "WaveShare SpotPear 3.5"
+echo "http://www.waveshare.com/wiki/3.5inch_RPi_LCD_(A)"
+echo ""
+echo "WaveShare SpotPear 4"
+echo "http://www.waveshare.com/wiki/4inch_RPi_LCD_(A)"
+echo "****************************************************"
+echo "If you have them installed, continue installation."
+echo "If not cancel (Ctrl+C) and go install drivers"
+echo "Wait 5 sec"
+sleep 5
+
+git clone git://github.com/AndrewFromMelbourne/raspi2fb.git
+cd raspi2fb
+mkdir build
+cd build
+cmake ..
+make
+
+debian_version=`egrep "jessie|wheezy" /etc/os-release`
+if [ `echo $debian_version|grep -c "jessie"` == 1 ]
+then
+   sudo make install
+   sudo cp ../raspi2fb@.service /etc/systemd/system/
+   sudo systemctl daemon-reload
+   sudo systemctl enable raspi2fb@1.service
+   sudo systemctl start raspi2fb@1
+elif [ `echo $debian_version|grep -c "wheezy"` == 1 ] 
+then
+   sudo make install
+   sudo cp ../raspi2fb.init.d /etc/init.d/raspi2fb
+   sudo update-rc.d raspi2fb defaults
+   sudo service raspi2fb start
+else
+   echo "Script checked your system and it's not Raspbian Wheezy or Jessie. Open text editor and try to write it by yourself"
+fi 
+
+echo "It's done. You should see your screen working."
